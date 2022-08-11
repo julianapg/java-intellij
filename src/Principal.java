eade19dbd8e9b14f9bbf9b029a819a9ff4ce3ee3
julianapg <julianapg92@gmail.com>
2022-08-11 18:20:00 -0500

    nuevo cambio .2

--- src/Principal.java
+++ src/Principal.java
@@ -2,5 +2,6 @@ public class Principal {
     public static void main(String[] args) {
         System.out.println("Hola");
         System.out.println("Grupo 1.0");
+        System.out.println("Prueba cambio 2");
     }
 }
