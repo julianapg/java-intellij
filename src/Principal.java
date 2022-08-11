ac6ef752b567f9f43f4b7595c8f0eddb7119ca31
julianapg <julianapg92@gmail.com>
2022-08-11 18:10:10 -0500

    nuevo print 1.0

--- src/Principal.java
+++ src/Principal.java
@@ -1,6 +1,6 @@
 public class Principal {
     public static void main(String[] args) {
         System.out.println("Hola");
-        System.out.println("Grupo 1");
+        System.out.println("Grupo 1.0");
     }
 }
